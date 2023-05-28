424e986e5eeeed85ebc596044807c05da839c859
Hal <haln_01@proton.me>
2023-05-28 13:34:06 -0700

    feat: log to stderr lib function

---
+++ lib/stderr.go
@@ -0,0 +1,17 @@
+package lib
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func StdErr(m error) {
+	now := time.Now()
+	year, month, date := now.UTC().Date()
+	hour := now.UTC().Hour()
+	minute := now.UTC().Minute()
+
+	str := fmt.Sprintf("%v-%v-%v %v:%v: ", month, date, year, hour, minute) + m.Error()
+	os.Stderr.Write([]byte(str))
+}
