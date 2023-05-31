21776e8d29abe214939fb5787a864f32b650c3f0
Matthew <matthew.rl.trent@gmail.com>
2023-05-31 05:47:04 -0700

    refactor: removes code used for debugging

--- features/auth/router.go
+++ features/auth/router.go
@@ -3,7 +3,6 @@ package auth
 import (
 	"confesi/db"
 	"confesi/lib/fire"
-	"confesi/middleware"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -16,9 +15,7 @@ type handler struct {
 
 func Router(mux *gin.RouterGroup) {
 	h := handler{db: db.New(), fb: fire.New()}
-	mux.Use(func(c *gin.Context) {
-		middleware.UsersOnly(c, h.fb.AuthClient, middleware.RegisteredFbUsers)
-	})
+
 	mux.POST("/register", func(c *gin.Context) {
 		h.handleRegister(c)
 	})
