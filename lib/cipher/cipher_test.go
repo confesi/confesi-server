ff0ef74fedf4515ce0a887336ee9bc5eb5831aec
Hal <haln_01@proton.me>
2023-06-03 12:59:48 -0700

    test: updated test scripts

--- lib/cipher/cipher_test.go
+++ lib/cipher/cipher_test.go
@@ -1,23 +1,20 @@
 package cipher
 
 import (
-	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCipher(t *testing.T) {
-	testStr := "test-user-id"
+	testStr := "test-user-input"
 
+	// encryption test
 	encrypted := Encrypt(testStr)
-	log.Fatal(encrypted, testStr)
 	assert.NotEqual(t, testStr, encrypted)
 
+	// description test
 	decrypted, err := Decrypt(encrypted)
-	log.Fatal(decrypted)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 	assert.Equal(t, testStr, decrypted)
 }
