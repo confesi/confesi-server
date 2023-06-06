822642d48c1ae0067b6360763d75712a683bb406
Matthew <matthew.rl.trent@gmail.com>
2023-06-06 01:30:35 -0700

    feat: adds trending algorithm

---
+++ lib/algorithm/trending_score.go
@@ -0,0 +1,51 @@
+package algorithm
+
+import "math"
+
+const (
+	baseTime          = 1686037996000 // base time in milliseconds (aka, the epoch for our trending calculations)
+	limitedMultiplier = 0.5           // multiplier for limited posts
+)
+
+// Calculates the trending score of a bit of content.
+func TrendingScore(upvotes int, downvotes int, currTime int, limited bool) float64 {
+	// calculate the time difference
+	timeDelta := float64(currTime - baseTime)
+
+	// calculate the total score
+	score := float64(upvotes - downvotes)
+
+	// calculate the y-factor
+	var y float64
+	if score > 0 {
+		y = 1
+	} else if score < 0 {
+		y = -1
+	} else {
+		y = 0
+	}
+
+	// calculate the z factor
+	var z float64
+	if math.Abs(score) >= 1 {
+		z = math.Abs(score)
+	} else {
+		z = 1
+	}
+
+	// check if the post needs to have limited reach
+	var limitFactor float64
+	if limited {
+		limitFactor = limitedMultiplier
+	} else {
+		limitFactor = 1
+	}
+
+	// calculate the trending score
+	result := (y*math.Log10(z) + (timeDelta / 45000))
+	if result < 0 {
+		return result
+	} else {
+		return result * limitFactor
+	}
+}
