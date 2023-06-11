56a3c7be337ea02b5c19bbaf10389fee8541e646
Hal <haln_01@proton.me>
2023-06-10 22:08:48 -0700

    feat(schools): query by school name

---
+++ features/schools/get_schools_test.go
@@ -0,0 +1,15 @@
+package schools
+
+import (
+	"confesi/db"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetSchoolByName(t *testing.T) {
+	h := handler{db.New()}
+	var schools []db.School
+	err := h.getBySchoolName(schools, "uvic", &Pagination{Offset: 1, Limit: 20})
+	assert.Nil(t, err)
+}
