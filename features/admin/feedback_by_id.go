f8d82b6bc42f5d15d7a1065b509d9643534b46ca
Chris <81205126+TalentedB@users.noreply.github.com>
2023-06-21 02:14:55 -0700

    fix: fixing stash

---
+++ features/admin/feedback_by_id.go
@@ -0,0 +1,29 @@
+package admin
+
+import (
+	"confesi/db"
+	"confesi/lib/response"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *handler) handleFeedbackID(c *gin.Context) {
+	feedbackID := c.Param("feedbackID")
+
+	_, err := strconv.ParseInt(feedbackID, 10, 64)
+	if err != nil {
+		response.New(http.StatusBadRequest).Err("invalid feedback id").Send(c)
+		return
+	}
+	feedback := db.Feedback{}
+	err = h.db.Model(&db.Feedback{}).Where("id = ?", feedbackID).First(&feedback).Error
+	if err != nil {
+		response.New(http.StatusInternalServerError).Err("server error").Send(c)
+	}
+
+	// if all goes well, send 200
+	response.New(http.StatusOK).Val(feedback).Send(c)
+	return
+}
