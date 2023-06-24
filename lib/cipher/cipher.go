8711c51d4ffc558d063e9a3d622d64118528e652
Hal <haln_01@proton.me>
2023-06-23 17:07:31 -0700

    fix: updated comments

--- lib/cipher/cipher.go
+++ lib/cipher/cipher.go
@@ -19,16 +19,19 @@ type Serializer interface {
 	// returns master key
 	// `[]byte` slice with length = 16
 	// either truncate, or pad.
+	// use something unique, but not the masking data itself
+	// ie: for masking user id, `MasterKey` can return `[]byte(userEmail)`
 	MasterKey() []byte
 
-	// Returns the masking []byte
+	// Returns the masking data
+	// ie `[]byte(user.id)`
 	Mask() []byte
 
 	// Write to struct
 	// data: ciphertext or plaintext
 	Serialize(data, nonce []byte)
 
-	// Returns cipherText, nonce, salt
+	// Returns ciphertext, nonce
 	// opposite of Serialize
 	Deserialize() ([]byte, []byte)
 }
