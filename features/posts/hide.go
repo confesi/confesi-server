f640385fa6c132807cf360c54566e8ff8ce902d4
Matthew <matthew.rl.trent@gmail.com>
2023-07-15 15:40:15 -0700

    feat: adds route to hide post

---
+++ features/posts/hide.go
@@ -0,0 +1,46 @@
+package posts
+
+import (
+	"confesi/db"
+	"confesi/lib/response"
+	"confesi/lib/utils"
+	"confesi/lib/validation"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *handler) handleHidePost(c *gin.Context) {
+	// validate request json
+	var req validation.HidePost
+	err := utils.New(c).Validate(&req)
+	if err != nil {
+		return
+	}
+
+	// get user token
+	token, err := utils.UserTokenFromContext(c)
+	if err != nil {
+		response.New(http.StatusInternalServerError).Err(serverError.Error()).Send(c)
+		return
+	}
+
+	// Update the "hidden" field on a post.
+	result := h.db.
+		Model(&db.Post{}).
+		Where("id = ? AND user_id = ?", req.PostID, token.UID).
+		Update("hidden", "true")
+
+	if result.Error != nil {
+		response.New(http.StatusInternalServerError).Err(serverError.Error()).Send(c)
+		return
+	}
+
+	if result.RowsAffected == 0 {
+		response.New(http.StatusBadRequest).Err("no post found with this ID").Send(c)
+		return
+	}
+
+	response.New(http.StatusOK).Send(c)
+	return
+}
