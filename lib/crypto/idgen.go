d42a8e219e087f3d2a0458a42d31a1c82726799c
Hal <haln_01@proton.me>
2023-07-23 20:08:39 -0700

    feat: id generator

---
+++ lib/crypto/idgen.go
@@ -0,0 +1,55 @@
+package crypto
+
+import (
+	"encoding/base64"
+	"encoding/binary"
+	"os"
+	"sync"
+	"time"
+)
+
+var (
+	counterMap map[string]uint16
+	m          sync.Mutex
+)
+
+func init() {
+	m = sync.Mutex{}
+	counterMap = make(map[string]uint16)
+}
+
+func RefreshCounterMap() {
+	for {
+		m.Lock()
+		counterMap = make(map[string]uint16)
+		m.Unlock()
+		time.Sleep(time.Second)
+	}
+}
+
+func NewID(id string) string {
+	d := []byte(id)
+	dLen := len(d)
+
+	bufSize := dLen
+	bufSize += 4 // time stamp utc is uint32
+	bufSize += 2 // pid is uint16
+	bufSize += 2 // counter variable is uint16
+
+	buf := make([]byte, bufSize)
+	copy(buf, d)
+
+	t := time.Now().UTC().Unix()
+	binary.BigEndian.PutUint32(buf[dLen:dLen+4], uint32(t))
+
+	pid := os.Getpid()
+	binary.BigEndian.PutUint16(buf[dLen+4:dLen+6], uint16(pid))
+
+	counter := counterMap[id]
+	m.Lock()
+	counterMap[id] = counter + 1
+	m.Unlock()
+	binary.BigEndian.PutUint16(buf[dLen+6:], uint16(counter))
+
+	return base64.StdEncoding.EncodeToString(buf)
+}
---
+++ lib/crypto/idgen_test.go
@@ -0,0 +1,35 @@
+package crypto
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCryptoGenID(t *testing.T) {
+	go RefreshCounterMap()
+
+	iter := math.MaxUint16
+	userID := "e0640eb6-29cd-11ee-be56-0242ac120002"
+	idSet := make(map[string]bool)
+
+	for i := 0; i < iter; i++ {
+		id := NewID(userID)
+		_, ok := idSet[id]
+		assert.False(t, ok)
+		idSet[id] = true
+	}
+
+	assert.Equal(t, iter, len(idSet))
+	time.Sleep(time.Second)
+
+	for i := 0; i < iter; i++ {
+		id := NewID(userID)
+		_, ok := idSet[id]
+		assert.False(t, ok)
+		idSet[id] = true
+	}
+	assert.Equal(t, iter*2, len(idSet))
+}
